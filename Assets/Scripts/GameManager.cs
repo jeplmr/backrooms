e153060d9000929af344435427b4ec4f31bcc52a
jeplmr <jeplmr@gmail.com>
2020-02-14 22:48:01 -0600

    working on shit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Maze mazePrefab; 
+    private Maze _mazeInstantance; 
+
+    private void Start(){
+        _mazeInstantance = Instantiate(mazePrefab) as Maze; 
+        BeginGame(); 
+    }
+
+    private void BeginGame(){
+        _mazeInstantance.Genrate(); 
+    }
+
+    private void RestartGame(){
+
+    }
+}
---
+++ Assets/Scripts/Maze.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maze : MonoBehaviour
+{
+    public int sizeX, sizeZ; 
+    public MazeCell cellPrefab; 
+    private MazeCell[,] cells; 
+
+    public void Genrate(){
+        cells = new MazeCell[sizeX, sizeZ]; 
+        for(int x = 0; x < sizeZ; x++){
+            for(int z = 0; z < sizeZ; z++){
+                CreateCell(x, z); 
+            }
+        }
+    }
+
+    private void CreateCell(int x, int z){
+        MazeCell newCell = Instantiate(cellPrefab) as MazeCell; 
+        cells[x,z] = newCell; 
+        newCell.name = "Maze Cell " + x + ", " + z; 
+        newCell.transform.parent = transform; 
+        newCell.transform.localPosition = new Vector3((x - sizeX) * 40f - 20f, 0f, (z - sizeZ) * 40f -20f);
+    }
+
+}
