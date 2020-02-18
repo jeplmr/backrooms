e794e28d46b655aab5e69cf0cd5a0dcc255fbf12
jeplmr <jeplmr@gmail.com>
2020-02-18 10:21:09 -0600

    removed some unused stuff
    did the thing

--- Assets/Scripts/Raycaster.cs
+++ Assets/Scripts/Raycaster.cs
@@ -9,7 +9,6 @@ public class Raycaster : MonoBehaviour
     [SerializeField]
     private PostProcessVolume _volume;
     private DepthOfField _DoF;
-    private Vector3 _hit; 
     public LayerMask mask; 
     
     void Start(){
@@ -19,7 +18,6 @@ public class Raycaster : MonoBehaviour
     void FixedUpdate(){
         RaycastHit hit; 
         if(Physics.Raycast(transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity, mask)){
-            _hit = hit.point; 
             FloatParameter newFocusDistance = new FloatParameter { value = Vector3.Distance(transform.position, hit.point) };
             if(newFocusDistance > 10f){
                 newFocusDistance = new FloatParameter {value = 10f}; 
@@ -27,12 +25,5 @@ public class Raycaster : MonoBehaviour
             _DoF.focusDistance.value = newFocusDistance;
         } 
     }
-
-/*
-    void OnDrawGizmos(){
-        Gizmos.DrawSphere(_hit, 0.5f);
-        Gizmos.DrawLine(transform.position, _hit); 
-    }
-*/
 
 }
