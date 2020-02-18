2dc9371ffa9f4543e9b9e87a8f70f4fca6b6ecc7
jeplmr <jeplmr@gmail.com>
2020-02-18 10:36:11 -0600

    added comments

--- Assets/Scripts/Raycaster.cs
+++ Assets/Scripts/Raycaster.cs
@@ -16,7 +16,9 @@ public class Raycaster : MonoBehaviour
     }
 
     void FixedUpdate(){
-        RaycastHit hit; 
+        //raycast from the main camera and check the distance to wherever the hit landed; i.e., where is the player looking and how far away is it?  
+        //use the mask to avoid hitting outselves; i.e., the player capsule, which needs to be assigned to its own layer to be masked
+        RaycastHit hit;
         if(Physics.Raycast(transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity, mask)){
             FloatParameter newFocusDistance = new FloatParameter { value = Vector3.Distance(transform.position, hit.point) };
             if(newFocusDistance > 10f){
